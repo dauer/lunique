bf6049ca5e6043ac5b997e162654039575596a18
Thomas Rasmussen <thomas.bregentved@gmail.com>
2012-11-13 10:58:16 +0100

    Added const to function definition (we don't want param altered)

--- src/lunique.c
+++ src/lunique.c
@@ -24,7 +24,7 @@ typedef enum {
     STD, TIME, TIMESAFE, RANDOM
 } Generators_t;
 
-static int _generator(lua_State *L, Generators_t g) {
+static int _generator(lua_State *L, const Generators_t g) {
     uuid_t uuid;
     char str[UUID_LEN];
     int ret = 0;
