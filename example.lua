77f16fbb44354f665699da59f76be654c9bb1b62
Thomas Rasmussen <thomas.bregentved@gmail.com>
2012-11-21 17:30:11 +0100

    Added example showing lunique.time()

--- example.lua
+++ example.lua
@@ -11,6 +11,7 @@ u2 = lunique.generate_time()
 print("----- generate_time() -----")
 print(u2)
 -- Get the time the UUID was generated
+print("----- time() -----")
 t1 = lunique.time(u2)
 print(t1)
 
@@ -19,6 +20,7 @@ u3, r1 = lunique.generate_time_safe()
 print("----- generate_time_safe() -----")
 print(u3, r1)
 -- Get the time the UUID was generated
+print("----- time() -----")
 t2 = lunique.time(u3)
 print(t2)
 
@@ -35,3 +37,11 @@ print("----- compare() -----")
 print(c1)
 print(c2)
 print(c3)
+
+-- Validate
+print("----- valid() -----")
+v1 = lunique.valid(u1)
+print(v1)
+-- invalid uuid, uses '#' instead of '-'
+v2 = lunique.valid('58b2195e#33f2#11e2#8819#001a4b53924b')
+print(v2)
